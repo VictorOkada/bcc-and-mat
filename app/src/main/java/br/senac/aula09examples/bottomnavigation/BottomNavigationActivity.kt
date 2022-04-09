431b1d2a37014ad082387ae425302c3fdc4cce29
victor.yokada <victoryassu@hotmail.com>
2022-04-08 22:01:36 -0300

    terminado

--- app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
+++ app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
@@ -2,7 +2,11 @@ package br.senac.aula09examples.bottomnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula09examples.R
 import br.senac.aula09examples.databinding.ActivityBottomNavigationBinding
+import br.senac.aula09examples.fragments.AlbumsFragment
+import br.senac.aula09examples.fragments.ArtistsFragment
+import br.senac.aula09examples.fragments.RecentsFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
@@ -12,7 +16,23 @@ class BottomNavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.artistas -> {
+                    val frag = ArtistsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.albuns -> {
+                    val frag = AlbumsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                else -> {
+                    val frag = RecentsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+            }
+            true
 
+        }
     }
-
 }
